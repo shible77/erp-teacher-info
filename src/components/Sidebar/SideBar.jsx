e18801a05b450971ab64ef572f037fec0490c84d
Shadakur Rahman Tamgid <tamgid311@gmail.com>
2024-03-31 13:39:33 +0600

    Initial Interface design

---
+++ src/components/Sidebar/SideBar.jsx
@@ -0,0 +1,195 @@
+import { NavLink } from "react-router-dom";
+import { FaBars, FaHome, FaLock, FaMoneyBill, FaUser, FaBook,FaUpload ,FaDownload} from "react-icons/fa";
+import { MdMessage,MdEventBusy } from "react-icons/md";
+import { BiAnalyse, BiSearch,BiCalendarEvent } from "react-icons/bi";
+import { BiCog } from "react-icons/bi";
+import { AiFillHeart, AiTwotoneFileExclamation,AiOutlineMessage } from "react-icons/ai";
+import { BsCartCheck,BsPencil,BsFileText } from "react-icons/bs";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import SidebarMenu from "./SidebarMenu";
+const routes = [
+  {
+    path: "/",
+    name: "Faculty",
+    icon: <FaUser />,
+  },
+  {
+    path: "/courses",
+    name: "Courses",
+    icon: <FaBook />,
+  },
+  {
+    path: "/leaves",
+    name: "Leaves",
+    icon: <MdEventBusy />,
+  },
+  {
+    path: "/meetings",
+    name: "Meetings",
+    icon: <BiCalendarEvent />,
+  },
+  {
+    path: "/editorials",
+    name: "Editorials",
+    icon: <BsFileText />,
+  },
+  {
+    path: "/chat",
+    name: "Chat",
+    icon: <AiOutlineMessage />,
+  },
+  {
+    path: "/file-manager",
+    name: "File Manager",
+    icon: <AiTwotoneFileExclamation />,
+    subRoutes: [
+      {
+        path: "/upload",
+        name: "Upload ",
+        icon: <FaUpload />,
+      },
+      {
+        path: "/download",
+        name: "Download",
+        icon: <FaDownload />,
+      },
+    ],
+  },
+];
+
+const SideBar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const inputAnimation = {
+    hidden: {
+      width: 0,
+      padding: 0,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    show: {
+      width: "140px",
+      padding: "5px 15px",
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+
+  const showAnimation = {
+    hidden: {
+      width: 0,
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    show: {
+      opacity: 1,
+      width: "auto",
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
+  return (
+    <>
+      <div className="main-container">
+        <motion.div
+          animate={{
+            width: isOpen ? "200px" : "45px",
+
+            transition: {
+              duration: 0.5,
+              type: "spring",
+              damping: 10,
+            },
+          }}
+          className={`sidebar `}
+        >
+          <div className="top_section">
+            <AnimatePresence>
+              {isOpen && (
+                <motion.h1
+                  variants={showAnimation}
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  className="logo"
+                >
+                  TeachersIMS
+                </motion.h1>
+              )}
+            </AnimatePresence>
+
+            <div className="bars">
+              <FaBars onClick={toggle} />
+            </div>
+          </div>
+          <div className="search">
+            <div className="search_icon">
+              <BiSearch />
+            </div>
+            <AnimatePresence>
+              {isOpen && (
+                <motion.input
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  variants={inputAnimation}
+                  type="text"
+                  placeholder="Search"
+                />
+              )}
+            </AnimatePresence>
+          </div>
+          <section className="routes">
+            {routes.map((route, index) => {
+              if (route.subRoutes) {
+                return (
+                  <SidebarMenu
+                    setIsOpen={setIsOpen}
+                    route={route}
+                    showAnimation={showAnimation}
+                    isOpen={isOpen}
+                  />
+                );
+              }
+
+              return (
+                <NavLink
+                  to={route.path}
+                  key={index}
+                  className="link"
+                  activeClassName="active"
+                >
+                  <div className="icon">{route.icon}</div>
+                  <AnimatePresence>
+                    {isOpen && (
+                      <motion.div
+                        variants={showAnimation}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className="link_text"
+                      >
+                        {route.name}
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </NavLink>
+              );
+            })}
+          </section>
+        </motion.div>
+
+        <main>{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default SideBar;
